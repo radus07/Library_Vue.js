c3ce559a320057d2d691b03b0debfe89fe6ce76f
Radu Balan <radu.balan.97@gmail.com>
2018-03-02 16:33:34 +0200

    Add PostgreSQL and generate models

---
+++ backend/models/roles.js
@@ -0,0 +1,16 @@
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('roles', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    tableName: 'roles'
+  })
+}
---
+++ backend/models/users.js
@@ -0,0 +1,40 @@
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'roles',
+        key: 'id'
+      }
+    }
+  }, {
+    tableName: 'users'
+  })
+}
